10ea4838af308e55219a4986c6c0a329bbfabb12
Divya-Bhavsar03 <divya.bhavsar@ucalgary.ca>
2026-04-04 10:08:38 -0600

    fix(map.js): Added CARTO

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -2,8 +2,9 @@
 
 const map = L.map('map', { zoomControl: true }).setView([51.0, -114.0], 7);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '© OpenStreetMap contributors, © CARTO',
+  maxZoom: 19
 }).addTo(map);
 
 setTimeout(() => { map.invalidateSize(); }, 100);
